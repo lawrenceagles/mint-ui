f80148085e92d78ca1a399f1f4e4c3e076b33704
lawrenceagles <lawrenceagles@gmail.com>
2021-09-23 16:03:25 +0100

    added Attributes class and generic attributes constraints

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ import { User } from './models/User';
 const user = new User({});
 
 user.set({ name: 'Jane Doe', age: 15 });
-
-user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -13,14 +13,4 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
 	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 	public events: Eventing = new Eventing(); // problem with this technique is that it is hard coded and we cannot swap out sub modules easily.
-
-	constructor(private data: UserProps) {}
-
-	get(propName: string): string | number {
-		return this.data[propName];
-	}
-
-	set(update: UserProps): void {
-		Object.assign(this.data, update); // overwrites everything in this.data with the update object
-	}
 }
